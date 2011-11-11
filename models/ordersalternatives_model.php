e963501ce65d24db7636ba1390a3b79998629374
Magnus Johnsson <magnusjjj@gmail.com>
2011-11-11 11:35:21 +0100

    Fixade så at ett räkningsproblem för hur många av ett betalningsalternativ fanns kvar inte skulle bli ett problem.

--- models/ordersalternatives_model.php
+++ models/ordersalternatives_model.php
@@ -2,7 +2,7 @@
 	class OrdersalternativesModel extends Model {  
 		function getAlternatives()
 		{
-			return $this->db->query("SELECT orders_alternatives.*, COUNT(orders_values.id) AS ammount_compare
+			return $this->db->query("SELECT orders_alternatives.*, COUNT(orders.id) AS ammount_compare
 			FROM orders_alternatives
 			LEFT OUTER JOIN orders_values ON orders_values.order_alternative_id = orders_alternatives.id
 			LEFT OUTER JOIN orders ON orders.id = orders_values.order_id AND orders.status = 'COMPLETED'
--- modules/ticket/itemtypes/section.php
+++ modules/ticket/itemtypes/section.php
@@ -1,13 +1,15 @@
 <fieldset>
 	<legend><?php echo $alternative['name'];?></legend>
 	<?php
-		foreach($alternatives_children[$alternative['id']] as $child)
-		{
-			if(!empty($child['template_override'])){
-				print_alternative($child, $alternatives_children);
-			} else {?>
-				<input type="checkbox" name="val[<?php echo $child['id'];?>]" value="y"/><?php echo $child['name'];?> (<?php echo $child['cost'];?> kr)<br/>
-	<?php
+		if(!empty($alternatives_children[$alternative['id']])){
+			foreach($alternatives_children[$alternative['id']] as $child)
+			{
+				if(!empty($child['template_override'])){
+					print_alternative($child, $alternatives_children);
+				} else {?>
+					<input type="checkbox" name="val[<?php echo $child['id'];?>]" value="y"/><?php echo $child['name'];?> (<?php echo $child['cost'];?> kr)<br/>
+		<?php
+				}
 			}
 		}
 	?>
