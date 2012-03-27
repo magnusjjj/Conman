08635d8a02f3028807a6bdb0ef888d4212c73195
Magnus Johnsson <magnusjjj@gmail.com>
2012-03-27 15:41:57 +0200

    Fixade lite med templatet så att det fungerar med google translate. Wtf Etu, sätta en size på body?

--- templates/newer/default.php
+++ templates/newer/default.php
@@ -9,8 +9,17 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Settings::$path;?>templates/newer/css_reset.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Settings::$path;?>templates/newer/box_model.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Settings::$path;?>templates/newer/style.css" />
+	<script>
+	function googleTranslateElementInit() {
+	  new google.translate.TranslateElement({
+	    pageLanguage: 'sv',
+	    floatPosition: google.translate.TranslateElement.FloatPosition.BOTTOM_RIGHT
+	  });
+	}
+	</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
   </head>
   <body>
+      <div id="centerpage">
 	<div id="topbar">
 	  <a class="blue"  href="<?php echo Router::url('/index/index');?>">Har du ett konto? <h1>Logga in här!</h1></a>
 	  <a class="black" href="<?php echo Router::url('/index/register_start');?>">Har du ingen biljett ännu? <h1>Köp en nu</h1></a>
@@ -25,5 +34,6 @@
 		?>
 	</div>
 	<!-- End Contentbox -->
+      </div>
   </body>
 </html>
