e173f9f5d491b2d1b0e551cb92c9d3251dea674b
Robert Tiinus <liinux@paneki>
2013-11-02 13:04:14 +0100

    Lade till automatisk generering av config.php

--- install.php
+++ install.php
@@ -25,9 +25,17 @@ fclose($handle);
 // Om post gjorts så validera config-data.
 $validationerror = false;
 if (!empty($_POST)) {
-	foreach ($questions as $question)
+	foreach ($questions as $question) {
+		if ($question[2] == "checkbox")
+			if (isset($_POST[$question[1]]))
+				$_POST[$question[1] = true;
+			else
+				$_POST[$question[1] = false;
+	
 		if (!isset($_POST[$question[1]]))
 			$validationerror = true;
+			
+	}
 }
 
 // Om valideringen misslyckats, eller config-data saknas, så fråga efter config-data.
@@ -87,7 +95,7 @@ foreach($questions as $question) {
 	}
 
 	if ($question[2] == "checkbox")
-		if(isset($_POST[$question[1]]))
+		if($_POST[$question[1]])
 			echo '<input type="checkbox" name="' . $question[1] . '" checked="yes">' . "\n";
 		else
 			echo '<input type="checkbox" name="' . $question[1] . '">' . "\n";
@@ -114,52 +122,17 @@ foreach($questions as $question) {
 	$filedata .= "\t\t\t'Test' => true,\n";
 	$filedata .= "\t\t\t'_application_email' => 'yourpaysonemail@mail.com');\n";
 
-	$filedata .= "\n";
-	
-	$filedata .= "\t\tstatic \$path = \"" . $_POST["path"] . "\";\n";
-
-	$filedata .= "\t\tstatic \$DbHost = \"" . $_POST["DbHost"] . "\";\n";
-	$filedata .= "\t\tstatic \$DbUser = \"" . $_POST["DbUser"] . "\";\n";
-	$filedata .= "\t\tstatic \$DbPassword = \"" . $_POST["DbPassword"] . "\";\n";
-	$filedata .= "\t\tstatic \$DbName = \"" . $_POST["DbName"] . "\";\n";
-	$filedata .= "\t\tstatic \$Url = \"" . $_POST["Url"] . "\";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$MembershipCost = " . $_POST["MembershipCost"] . ";\n";
-	$filedata .= "\t\tstatic \$ConEnds = \"" . $_POST["ConEnds"] . "\";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$MailFrom = \"" . $_POST["MailFrom"] . "\";\n";
-	$filedata .= "\t\tstatic \$SMTPServer = \"" . $_POST["SMTPServer"] . "\";\n";
-	$filedata .= "\t\tstatic \$SMTPPort = " . $_POST["SMTPPort"] . ";\n";
-	$filedata .= "\t\tstatic \$SMTPUser = \"" . $_POST["SMTPUser"] . "\";\n";
-	$filedata .= "\t\tstatic \$SMTPPassword = \"" . $_POST["SMTPPassword"] . "\";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$ErrorReporting = " . $_POST["ErrorReporting"] . ";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$EventName = \"" . $_POST["EventName"] . "\";\n";
-	$filedata .= "\t\tstatic \$Society = \"" . $_POST["Society"] . "\";\n";
-	$filedata .= "\t\tstatic \$Template = \"" . $_POST["Template"] . "\";\n";
-	$filedata .= "\t\tstatic \$AllowPayson = " . $_POST["AllowPayson"] . ";\n";
-	$filedata .= "\t\tstatic \$RequireEmail = " . $_POST["RequireEmail"] . ";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$CustomerserviceUrl = \"" . $_POST["CustomerserviceUrl"] . "\";\n";
-	$filedata .= "\t\tstatic \$CustomerserviceEmail = \"" . $_POST["CustomerserviceEmail"] . "\";\n";
-	$filedata .= "\t\tstatic \$TechEmail = \"" . $_POST["TechEmail"] . "\";\n";
-	$filedata .= "\t\tstatic \$StatutesUrl = \"" . $_POST["StatutesUrl"] . "\";\n";
-	$filedata .= "\t\tstatic \$TermsUrl = \"" . $_POST["TermsUrl"] . "\";\n";
-
-	$filedata .= "\n";
-
-	$filedata .= "\t\tstatic \$BarKey = \"" . $_POST["BarKey"] . "\";\n";
+	foreach($questions as $question) {
+		if ($curgroup != $question[0]) {
+			$curgroup = $question[0];
+			$filedata .= "\n";
+		}
+		
+		if ($question[2] == "number" || $question[2] == "checkbox")
+			$filedata .= "\t\tstatic \$" . $question[1] . " = " . $_POST[$question[1]] . ";\n";
+		else
+			$filedata .= "\t\tstatic \$" . $question[1] . " = \"" . $_POST[$question[1]] . "\";\n";
+	}
 
 	$filedata .= "\t}\n";
 
